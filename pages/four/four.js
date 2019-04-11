cd874cce90c0330bac8c58cd5e0d1ac923fca839
wwj <2589856016@qq.com>
2019-04-11 16:09:08 +0800

    显示页面生命周期

--- pages/four/four.js
+++ pages/four/four.js
@@ -12,35 +12,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("four页面onLoad")
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log("four页面onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("four页面onShow")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("four页面onHide")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log("four页面onUnload")
   },
 
   /**
